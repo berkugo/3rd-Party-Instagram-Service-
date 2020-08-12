f5c85e340a680f01adc8c1c76346c8a783ee51f3
mandolorian <mandolorian@localhost.localdomain>
2020-08-13 00:25:16 +0300

    session-id

--- insta.770f4f0ca51c93a437a3ab8c0a91f5ca.sh
+++ insta.770f4f0ca51c93a437a3ab8c0a91f5ca.sh
@@ -1,4 +1,4 @@
-cookie="sessionid=11295935212%3A6pSXrttYWwNLMf%3A1"
+cookie="sessionid={YOUR_SESSION_ID}"
 
 resp="`curl -s -b \"$cookie\" https://www.instagram.com/$1/?__a=1`" 
 echo $resp | jq  --raw-output   '.graphql.user |   {fullname: .full_name, follower_count: .edge_followed_by.count ,user_id: .id,business: .is_business_account,is_recent: .is_joined_recently,business_category_name: .business_category_name,overall_category_name: .overall_category_name,category_enum:  .category_enum,private: .is_private,verified: .is_verified}'
